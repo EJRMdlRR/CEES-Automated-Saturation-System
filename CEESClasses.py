a26112ae885a5d25ec5e7197942d5b6664fd0501
EJRMdlRR <48624761+EJRMdlRR@users.noreply.github.com>
2020-08-31 20:08:57 -0400

    Add files via upload

---
+++ CEESClasses.py
@@ -0,0 +1,167 @@
+import sys
+import time
+import select
+import datetime
+
+class Experiment: 
+    # Constructor
+    def __init__(self, title = 'Auto', exp = '000', user = 'Default', spd = 2, visc = 50, notes=''):
+        # Independednt Data
+        self.title = title
+        self.experiment = exp
+        self.date = datetime.datetime.now()
+        self.user = user
+        self.secPerDrops = spd
+        self.viscosity = visc
+        self.iNotes = notes
+        self.filename = self.title + "_" + self.experiment + ".txt"
+        self.began = time.time()
+        
+        # Drop Data
+        self.optimalVoltage = 0
+        self.dropData = []
+        self.lastDrop = 0
+        self.dropSum = 0
+        self.volts = 5
+        self.cVolts = 5
+        
+        # Noise data
+        self.noiseData = []
+        self.noiseLength = 0   # number of noise frames
+        self.noiseSum = 0     # sum of noise frames
+        
+        # Misc
+        self.notes = []
+
+    # Getters
+    def vGet(self): return self.volts
+    def cGet(self): return self.cVolts
+    def tSinceDrop(self): return time.time() - self.lastDrop
+    def dropAvg(self): return self.dropSum / len(self.dropData)
+    def pixAvg(self): return self.noiseSum / self.noiseLength
+    
+    # Setters
+    def vEqualize(self):
+        while(self.cVolts > self.volts):
+            self.cVolts -= 81
+            
+            # Bound checking
+            if self.cVolts > 4055: self.cVolts = 4055
+            elif self.cVolts < 5:  self.cVolts = 5
+            if (self.cVolts < self.volts): self.cVolts = self.volts
+            
+            time.sleep(0.05) 
+        self.lastDrop = 0
+        
+    def vSet(self, key):
+        if key == 0:   self.volts = input("Please enter a voltage: ")
+        elif key == 1: self.volts += 5
+        elif key == 2: self.volts -= 5
+        elif key > 2: self.volts = key
+        
+        # Bound checking
+        if self.volts > 4055: self.volts = 4055
+        elif self.volts < 5:  self.volts = 5
+        
+        self.cVolts = self.volts
+        print("Voltage: {:.2f}%".format(100 * ((self.volts - 5)/ 4050)))
+        
+    def cSet(self, key):
+        if key == 0:   self.cVolts = input("Please enter a voltage: ")
+        elif key == 1: self.cVolts += 81
+        elif key == 2: self.cVolts -= 81
+        elif key > 2: self.cVolts = key
+        
+        # Bound checking
+        if self.cVolts > 4055: self.cVolts = 4055
+        elif self.cVolts < 5:  self.cVolts = 5
+        if (self.cVolts < self.volts): self.cVolts = self.volts
+        print("Voltage: {:.2f}%".format(100 * ((self.cVolts - 5) / 4050)))      
+    
+    def dSet(self):
+        self.optimalVoltage = self.volts
+        return True
+    
+    # Miscellaneous
+    def addNoise(self, frameNumber, nonzero):
+        self.noiseLength += 1
+        self.noiseSum += nonzero
+        self.noiseData.append((frameNumber, nonzero))
+        return self.noiseSum / self.noiseLength
+        
+    def addDrop(self, frameNumber, nonzero):
+        # data storage
+        self.dropData.append([time.time() - self.began, time.time() - self.lastDrop, frameNumber, self.noiseSum / self.noiseLength, nonzero, self.volts])
+        self.dropSum += nonzero
+        
+        # voltage calculations
+        if (self.lastDrop != 0):
+            self.cVolts = self.volts = self.optimalVoltage + ((time.time() - self.lastDrop) - self.secPerDrops) * self.viscosity
+            print("{:.2f}s since last drop".format(time.time() - self.lastDrop))
+            print("Voltage: {:.2f}%".format(100 * (self.volts / 4050)))
+        self.lastDrop = time.time()
+        
+        # Bound checking
+        if self.volts > 4055: self.volts = 4055
+        elif self.volts < 5:  self.volts = 5
+        
+        return self.volts
+    
+    def input_with_timeout(self, prompt, timeout):
+        print(prompt)
+        ready, _, _ = select.select([sys.stdin], [],[], timeout)
+        if ready:
+            return sys.stdin.readline().rstrip('\n') # expect stdin to be line-buffered
+        return "None"    
+
+    def addNotes(self, frameNumber):
+        noteFrame = self.input_with_timeout("Notes: ", 30)
+        self.notes.append("Notes at frame [{}]: {}".format(frameNumber) + noteFrame)
+    
+    def finalNotes(self):
+        self.fNotes = "Final Notes: " + self.input_with_timeout("Final Notes: ", 30)
+    
+    def terminate(self, success):
+        if(success): s = "Succesful"
+        else: s = "Unsuccesful"
+        
+        # Open file in write mode
+        dropFile = open(self.filename,"w")
+        
+        # Write initial information
+        dropFile.write("Title: {}\n".format(self.title)) 
+        dropFile.write("Experiment Number: {}\n".format(self.experiment))
+        dropFile.write("Date: {}\n".format(self.date))
+        dropFile.write("User/s: {}\n".format(self.user))
+        dropFile.write("Viscosity: {}\n".format(self.viscosity)) 
+        dropFile.write("DPS: {}\n".format(self.secPerDrops)) 
+        dropFile.write("Notes: {}\n".format(self.iNotes))
+        dropFile.write("TotalTime\tTimeSinceDrop\tFrame\tMovingPixelAvg\tMovingPixels\tVoltage")
+        
+        # Write drop data
+        for drop in self.dropData:
+            dropFile.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(drop[0], drop[1], drop[2], drop[3], drop[4], drop[5]))
+            
+        # Write final information
+        dropFile.write("Tank Emptied: {}\n".format(s))
+        dropFile.write("Final voltage: {}\n".format(self.volts))
+        dropFile.write("Total drops: {}\n".format(len(self.dropData)))
+        dropFile.write("Average pixel delta: {}\n".format(self.noiseSum / self.noiseLength))
+        if (len(self.dropData) == 0): dropAverage = 0
+        else: dropAverage = self.dropAvg()
+        dropFile.write("Average drop delta: {}\n".format(dropAverage))
+        for note in self.notes:
+            dropFile.write(note)
+        dropFile.write(self.fNotes)
+        
+        # Close drop file
+        dropFile.close()
+        
+        # Write separate file for noise data
+        noiseFile = open(self.title + "_" + self.experiment + "_Noise", "w")
+        
+        # Write noise
+        for noise in self.noiseData:
+            noiseFile.write("{}\t{}\n".format(noise[0], noise[1]))
+            
+        noiseFile.close()
---
+++ vision_code_4.py
@@ -0,0 +1,193 @@
+# Import libraries
+from multiprocessing import Process
+from CEESClasses import Experiment
+import adafruit_mcp4725
+import numpy as np
+import cv2 as cv
+import datetime
+#import board
+import busio
+import time
+
+# Define functions
+def initialize():
+    # Initialize I2C bus and MCP4725 board
+    #i2c = busio.I2C(board.SCL, board.SDA)     
+    #dac = adafruit_mcp4725.MCP4725(i2c)    
+    
+    # Initialize Experiment object
+    title = "v4" #input("Title: ")
+    exp = input("Experiment #: ")
+    user = "I"   #input("User/s: ")
+    spd = 2      #float(input("Desired drop Rate: "))
+    visc = 60    #float(input("Liquid Viscosity: "))
+    notes = "N/A"#input("Enter any additional notes: ")
+    e = Experiment(title, exp, user, spd, visc, notes)
+    return e
+
+def imageProcessing(backSub, frame, coordinates):
+    north, south, east, west = coordinates
+    
+    gray = cv.cvtColor(rect_img, cv.COLOR_BGR2GRAY)
+    fgMask = backSub.apply(gray) #this is basically 80% of the program right here
+    fgMask_RGB = cv.cvtColor(fgMask, cv.COLOR_GRAY2RGB)
+    
+    cv.rectangle(frame, (10, 2), (100,20), (0,0,0), -1)
+    cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5 , (255,255,255))
+    
+    #Replacing the sketched image on Region of Interest
+    frame[north: south, west : east] = fgMask_RGB 
+    nonzero = cv.countNonZero(fgMask)  
+    return frame, nonzero
+
+def parallelize(function, arguments = None):
+    t = Process(target = function, args = arguments)
+    t.start()
+    
+def fSet(coordinates, key):
+    validKeys = [ord('w'), ord('W'), ord('a'), ord('A'), ord('s'), ord('S'),
+                 ord('d'), ord('D'), ord('i'), ord('I'), ord('j'), ord('J'),
+                 ord('k'), ord('K'), ord('l'), ord('L'),]
+    north, south, east, west = coordinates
+    
+    # Top-Right Corner
+    if key == ord('w') or key == ord('W'):   north -= 5
+    elif key == ord('a') or key == ord('A'): west -= 5
+    elif key == ord('s') or key == ord('S'): north += 5
+    elif key == ord('d') or key == ord('D'): west += 5
+    
+    # Bottom-Left Corner
+    elif key == ord('i') or key == ord('I'): south -= 5
+    elif key == ord('j') or key == ord('J'): east -= 5
+    elif key == ord('k') or key == ord('K'): south += 5
+    elif key == ord('l') or key == ord('L'): east += 5
+    
+    # Bound checking
+    if (north < 0): north = 0
+    elif (north > height): north = height
+    if (south < 0): south = 0
+    elif (south > height): south = height
+    if (east< 0): east = 0
+    elif (east > width): east = width    
+    if (west < 0): west = 0
+    elif (west > width): west = width
+    
+    if key in validKeys: print("Coordinates: ({}, {}), ({}, {})".format(north, west, south, east))
+    return [north, south, east, west]
+
+def summary():
+    print("Final voltage: {:.2f}".format(voltage))
+    print("Total drops: {}".format(num_drops))
+    print("Average pixel delta: {:.2f}".format(pix_avg))
+    print("Average drop delta: {:.2f}".format(dp_avg))    
+
+def terminationProcedure(volts):    
+    volts =int(volts/10)*10
+    while (volts >= 10):
+        volts -= 5
+        #dac.raw_value = volts
+        time.sleep(0.1)        
+
+if __name__ == '__main__':
+    # Initialize settings
+    e = initialize()
+    frames= 0
+    waitFrame = 0
+    liquid = True
+    mutiny = False
+    success = False
+    defaults = False
+    calibration = False
+    
+    # Open video feed
+    backSub = cv.createBackgroundSubtractorMOG2(history = 40, varThreshold = 60, detectShadows = False)
+    capture = cv.VideoCapture(0) # 0 - laptop webcam; 1 - USB webcam; "cv.samples.findFileOrKeep(args.input))" - file
+    if not capture.isOpened:
+        print('Unable to open: ' + args.input)
+        exit(0)
+        
+    # get image dimensions
+    height = int(capture.get(4))
+    width = int(capture.get(3))
+    coords = [0, height, width, 0]
+
+    # clog values
+    timeOpen = 0
+    latencyDelay = 0
+    clogged = False
+    
+    while (liquid):
+        # Takes frame input from camera
+        ret, frame = capture.read()
+        if frame is None:
+            break    
+      
+        # Rectangle marker
+        r = cv.rectangle(frame, (coords[3], coords[0]), (coords[2], coords[1]), (100, 50, 200), 3)
+        rect_img = frame[coords[0]:coords[1], coords[3]: coords[2]]
+        
+        # Main processing of program
+        if (defaults):
+            frames += 1
+            frame, delta = imageProcessing(backSub, frame, coords)
+            if(calibration):
+                if(delta > pixAvg * 5):
+                    if (e.cGet() > e.vGet()): e.vEqualize()                     
+                    if (waitFrame == 0): e.addDrop(frames, delta)
+                    elif (waitFrame < 10): waitFrame += 1 # change to if frame before was drop, no drop
+                    else: waitFrame = 0
+                    timeOpen = 0
+                    clogged = False
+                else:
+                    # clog protocol
+                    # while there is no drop, increase voltage every 10s (to account for latency)
+                    # if 60s at max voltage with no drop pass, terminate
+                    if (e.tSinceDrop() > 30):
+                        if (not clogged): 
+                            latencyDelay = time.time()
+                            clogged = True
+                        if (time.time() - latencyDelay > 10):
+                            print("{:.2f}".format(time.time() - latencyDelay))
+                            if (e.cGet() == 4054): timeOpen += 1
+                            else: e.cSet(1)
+                            if (e.cGet() >= 4055): e.cSet(4054)
+                            clogged = False
+                        if (timeOpen >= 6):
+                            e.vSet(4055)
+                            success = True
+                            liquid = False
+                    else: pixAvg = e.addNoise(frames, delta)                   
+            else: 
+                mutiny = True
+                # dac.raw_value = 5
+                time.sleep(0.01)
+                if (frames < 250): 
+                    pixAvg = e.addNoise(frames, delta)  
+                else: 
+                    print("CALIBRATION COMPLETE")
+                    mutiny = False
+                    calibration = True
+    
+        # Show the image in a resizeable frame
+        cv.namedWindow('Frame',cv.WINDOW_NORMAL)
+        cv.imshow('Frame', frame)      
+        k = cv.waitKey(60) & 0xFF
+
+        # Checks for keypresses
+        if ((k == ord('q')) or (k == ord('Q'))): liquid = False
+        elif (k == ord('0')): defaults = e.dSet()                        # Confirm default voltage and rectangle size and location
+        elif ((k == ord('r')) or (k == ord('R'))): defaults = False      # Don't save data while default voltage and rectangle values are reset
+        elif ((k == ord('c')) or (k == ord('C'))): calibration = False   # Calibration sequence restarts (100 frames to average noise level)
+        elif ((k == ord('n')) or (k == ord('N'))): parallelize(e.addNotes, (frames,)) # Insert additional notes, find out how to parallelize
+        elif ((k == ord('e')) or (k == ord('E'))): e.vSet(0)
+        elif (k == ord('+')): e.vSet(1)
+        elif (k == ord('-')): e.vSet(2)
+        elif (k != 0xFF): coords = fSet(coords, k)
+        
+        if (not mutiny): dac = 1 #dac.raw_value = e.vGet()
+    capture.release()
+    cv.destroyAllWindows()
+    parallelize(terminationProcedure, (e.vGet(),))
+    e.finalNotes()
+    e.terminate(success)
