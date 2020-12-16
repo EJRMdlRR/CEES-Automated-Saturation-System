6a2e6eed179d89149bcc47210e27cb31bee253ad
Ivan <ivanbenji2@gmail.com>
2020-12-16 00:29:33 -0400

    Working. Drop controls need tweaking

--- vision_code_4.py
+++
@@ -1,5 +0,0 @@
-from experiment import Experiment
-
-
-if __name__ == '__main__':
-    Experiment("ASS_v5").main()
---
+++ vision_code_6.py
@@ -0,0 +1,5 @@
+from experiment import Experiment
+
+
+if __name__ == '__main__':
+    Experiment("ASS_v5").main()
